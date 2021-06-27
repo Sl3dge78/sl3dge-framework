f469bb57905f0a44669c79ebf02c2740649e9c18
Sl3dge78 <45926471+Sl3dge78@users.noreply.github.com>
2021-06-27 10:38:15 +0200

    Allocate big buffers instead of heap

--- module.h
+++ module.h
@@ -26,10 +26,10 @@ Win32CloseModule(&game_module);
 */
 
 typedef struct Module {
-    char *meta_path;
+    char meta_path[64];
     FILETIME last_write_time;
     HMODULE dll;
-    char *tmp_dll;
+    char tmp_dll[64];
 } Module;
 
 internal inline FILETIME Win32GetLastWriteTime(const char *file_name) {
@@ -37,7 +37,7 @@ internal inline FILETIME Win32GetLastWriteTime(const char *file_name) {
 
     WIN32_FIND_DATA data;
     HANDLE handle = FindFirstFile(file_name, &data);
-    if (handle != INVALID_HANDLE_VALUE) {
+    if(handle != INVALID_HANDLE_VALUE) {
         last_write_time = data.ftLastWriteTime;
         FindClose(handle);
     }
@@ -47,8 +47,8 @@ internal inline FILETIME Win32GetLastWriteTime(const char *file_name) {
 
 internal bool Win32ShouldReloadModule(Module *module) {
     FILETIME current_time = Win32GetLastWriteTime(module->meta_path);
-    if (CompareFileTime(&module->last_write_time,
-                        &current_time)) { // Has the file changed ?
+    if(CompareFileTime(&module->last_write_time,
+                       &current_time)) { // Has the file changed ?
         module->last_write_time = current_time;
         return true;
     }
@@ -63,19 +63,19 @@ internal bool Win32LoadModule(Module *module, const char *name) {
     const u32 path_length = strlen(path);
 
     u32 meta_length = path_length + name_length + strlen(".meta") + 1;
-    module->meta_path = (char *)calloc(meta_length, sizeof(char));
-    strcat_s(module->meta_path, meta_length, path);
-    strcat_s(module->meta_path, meta_length, name);
-    strcat_s(module->meta_path, meta_length, ".meta");
+    strcat_s(module->meta_path, path);
+    strcat_s(module->meta_path, name);
+    strcat_s(module->meta_path, ".meta");
 
     u32 orig_length = path_length + name_length + strlen(".dll") + 1;
-    char *orig_dll = (char *)calloc(orig_length, sizeof(char));
-    strcat_s(orig_dll, orig_length, path);
-    strcat_s(orig_dll, orig_length, name);
-    strcat_s(orig_dll, orig_length, ".dll");
+
+    char orig_dll[64] = {};
+    strcat_s(orig_dll, path);
+    strcat_s(orig_dll, name);
+    strcat_s(orig_dll, ".dll");
 
     u32 temp_length = path_length + name_length + strlen("_temp.dll") + 1;
-    module->tmp_dll = (char *)calloc(temp_length, sizeof(char));
+
     strcat_s(module->tmp_dll, temp_length, path);
     strcat_s(module->tmp_dll, temp_length, name);
     strcat_s(module->tmp_dll, temp_length, "_temp.dll");
@@ -85,26 +85,24 @@ internal bool Win32LoadModule(Module *module, const char *name) {
     // copy only if the file exists. This will reload the previous _tmp
     WIN32_FIND_DATA f;
     HANDLE hdl = FindFirstFile(orig_dll, &f);
-    if (hdl != INVALID_HANDLE_VALUE) {
+    if(hdl != INVALID_HANDLE_VALUE) {
         FindClose(hdl);
         DeleteFile(module->tmp_dll);
-        if (!CopyFile(orig_dll, module->tmp_dll, FALSE)) {
+        if(!CopyFile(orig_dll, module->tmp_dll, FALSE)) {
             DWORD dw = GetLastError();
             SDL_LogError(0, "Error copying module %s, %d", name, dw);
         }
     } else {
-        SDL_LogWarn(0, "%s.dll doesn't exist, reloading the previous module.",
-                    name);
+        SDL_LogWarn(0, "%s.dll doesn't exist, reloading the previous module.", name);
     }
 
     module->dll = LoadLibrary(module->tmp_dll);
 
-    if (!module->dll) {
+    if(!module->dll) {
         SDL_LogError(0, "Unable to load module %s", name);
         return false;
     }
 
-    free(orig_dll);
     module->last_write_time = Win32GetLastWriteTime(module->meta_path);
 
     SDL_Log("Module loaded : %s", name);
@@ -113,7 +111,5 @@ internal bool Win32LoadModule(Module *module, const char *name) {
 
 internal void Win32CloseModule(Module *module) {
     FreeLibrary(module->dll);
-    free(module->tmp_dll);
-    free(module->meta_path);
 }
 #endif // SL_MODULE_H
