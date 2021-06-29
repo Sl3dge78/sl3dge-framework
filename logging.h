54acae947532c16fc65da516e0e710cc9bf15d47
Sl3dge78 <45926471+Sl3dge78@users.noreply.github.com>
2021-06-30 00:01:09 +0200

    colors on printf!

--- logging.h
+++ logging.h
@@ -8,10 +8,19 @@
 
 #define MAX_LOG_LENGTH 256
 
-#define sTrace(message, ...) LogOutput(0, message, ##__VA_ARGS__)
-#define sLog(message, ...) LogOutput(1, message, ##__VA_ARGS__)
-#define sWarn(message, ...) LogOutput(2, message, ##__VA_ARGS__)
-#define sError(message, ...) LogOutput(3, message, ##__VA_ARGS__);
+#ifdef DEBUG
+#define sTrace(message, ...) LogOutput(LOG_LEVEL_TRACE, message, ##__VA_ARGS__)
+#define sLog(message, ...) LogOutput(LOG_LEVEL_LOG, message, ##__VA_ARGS__)
+#define sWarn(message, ...) LogOutput(LOG_LEVEL_WARN, message, ##__VA_ARGS__)
+#define sError(message, ...) LogOutput(LOG_LEVEL_ERROR, message, ##__VA_ARGS__)
+#else
+#define sTrace(message, ...)
+#define sLog(message, ...)
+#define sWarn(message, ...)
+#define sError(message, ...)
+#endif
+
+enum LogLevel { LOG_LEVEL_TRACE, LOG_LEVEL_LOG, LOG_LEVEL_WARN, LOG_LEVEL_ERROR };
 
 typedef void LogCallback_t(const char *message, const u8 level);
 typedef LogCallback_t *PFN_LogCallback;
@@ -20,7 +29,14 @@ LogCallback_t DefaultLog;
 global PFN_LogCallback callback = &DefaultLog;
 
 void DefaultLog(const char *message, const u8 level) {
+    switch(level) {
+    case LOG_LEVEL_ERROR: printf("\033[0;31m"); break;
+    case LOG_LEVEL_WARN: printf("\033[0;33m"); break;
+    case LOG_LEVEL_LOG: printf("\033[0m"); break;
+    case LOG_LEVEL_TRACE: printf("\033[1;30m"); break;
+    }
     printf("%s", message);
+    printf("\033[0m");
 }
 
 void sLogSetCallback(PFN_LogCallback cb) {
