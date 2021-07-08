13bf6430d87ac69ee407fd693f0b3c999459e149
Sl3dge78 <45926471+Sl3dge78@users.noreply.github.com>
2021-07-08 21:50:21 +0200

    Change log level with sLogLevel

--- sLogging.h
+++ sLogging.h
@@ -34,14 +34,17 @@
         (byte & 0x10 ? '1' : '0'), (byte & 0x08 ? '1' : '0'), (byte & 0x04 ? '1' : '0'),           \
         (byte & 0x02 ? '1' : '0'), (byte & 0x01 ? '1' : '0')
 
-enum LogLevel {
+typedef enum LogLevel {
 
     LOG_LEVEL_TRACE,
     LOG_LEVEL_LOG,
     LOG_LEVEL_WARN,
     LOG_LEVEL_ERROR,
     LOG_LEVEL_NONE = LOG_LEVEL_LOG,
-};
+} LogLevel;
+
+global LogLevel LOG_LEVEL = LOG_LEVEL_WARN;
+
 enum LogColor { LOG_COLOR_WHITE, LOG_COLOR_RED, LOG_COLOR_YELLOW, LOG_COLOR_GREY, LOG_COLOR_GREEN };
 
 typedef void LogCallback_t(const char *message, const u8 level);
@@ -65,8 +68,14 @@ void sLogSetCallback(PFN_LogCallback cb) {
     callback = cb;
 }
 
+void sLogLevel(LogLevel level) {
+    LOG_LEVEL = level;
+}
+
 // Outputs string format to console. Adds a new line.
 void sLogOutputLine(u8 level, const char *fmt, ...) {
+    if(level < LOG_LEVEL)
+        return;
     va_list args;
     va_start(args, fmt);
     char buffer[MAX_LOG_LENGTH];
@@ -80,6 +89,8 @@ void sLogOutputLine(u8 level, const char *fmt, ...) {
 }
 
 void sLogOutput(u8 level, const char *fmt, ...) {
+    if(level < LOG_LEVEL)
+        return;
     va_list args;
     va_start(args, fmt);
     char buffer[MAX_LOG_LENGTH];
